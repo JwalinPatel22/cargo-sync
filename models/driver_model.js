d7bcf147f21ee572f65efdc68155d9e3d2bdb2c5
Jwalin Patel <jwalinpatel22@gmail.com>
2024-09-12 21:49:57 +0530

    minor change

--- models/driver_model.js
+++ models/driver_model.js
@@ -2,13 +2,7 @@ import mongoose from "mongoose";
 import User from "./base_user_model.js";
 
 const DriverSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phoneNumber: { type: Number, required: true },
-  address: { type: String, required: true },
   licenseNumber: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now },
 });
 
 export const Driver = User.discriminator("Driver", DriverSchema);
