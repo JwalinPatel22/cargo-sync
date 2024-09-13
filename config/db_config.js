070be01d85bd707b3387c946b1a98c596a611a40
Jwalin Patel <jwalinpatel22@gmail.com>
2024-09-13 19:51:40 +0530

    dealer_model

--- config/db_config.js
+++ config/db_config.js
@@ -5,13 +5,13 @@ dotenv.config(); // Load environment variables from .env file
 
 const connectDB = async () => {
   try {
-    // if (!process.env.MONGO_URL) {
-    //   throw new Error(
-    //     "MongoDB URL is missing. Make sure it's defined in the .env file."
-    //   );
-    // }
+    if (!process.env.MONGO_URL) {
+      throw new Error(
+        "MongoDB URL is missing. Make sure it's defined in the .env file."
+      );
+    }
 
-    const conn = await mongoose.connect("mongodb://localhost:27017/sihD");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
       `Connected to MongoDB Atlas Database at ${mongoose.connection.host}`
     );
--- models/dealer_model.js
+++ models/dealer_model.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import  User  from "./base_user_model.js";
+import User from "./base_user_model.js";
 
 const DealerSchema = new mongoose.Schema({
   address: { type: [String], required: true },
-  truck: { type: [mongoose.Schema.Types.ObjectId], ref: "Truck" },
+  truck: { type: [mongoose.Schema.Types.ObjectId] },
 });
 
 export const Dealer = User.discriminator("Dealer", DealerSchema);
